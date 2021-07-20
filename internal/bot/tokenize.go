187238fb17e1aa197dd632525bbcf92fb0a9190b
Vitaly Karpenko <vitkarpenko@gmail.com>
2021-07-20 23:21:46 +0300

    Messages tokenization.

---
+++ internal/bot/tokenize.go
@@ -0,0 +1,23 @@
+package bot
+
+import (
+	"regexp"
+	"strings"
+)
+
+var anySeparatorRegexp = regexp.MustCompile("[^а-яА-Я\\w\\d]+")
+
+// tokenize splits input by any non-alphabetic/non-numeric characters and lowercases tokens.
+func tokenize(message string) []string {
+	splitted := anySeparatorRegexp.Split(message, -1)
+	result := lowerCase(splitted)
+	return result
+}
+
+func lowerCase(tokens []string) []string {
+	result := make([]string, 0, len(tokens))
+	for _, token := range tokens {
+		result = append(result, strings.ToLower(token))
+	}
+	return result
+}
