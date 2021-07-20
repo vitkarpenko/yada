3cd084cdb56f3c0dd5e8e26d15fe50e2b97fb750
Vitaly Karpenko <vitkarpenko@gmail.com>
2021-07-20 18:11:46 +0300

    Playing with slash commands WIP.

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module yada
 go 1.16
 
 require (
-	github.com/diamondburned/arikawa/v2 v2.1.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/bwmarrin/discordgo v0.23.3-0.20210627161652-421e14965030 // indirect
 	github.com/joho/godotenv v1.3.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- main.go
+++ main.go
@@ -1,30 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
-	"yada/internal"
+	"yada/internal/commands"
 
-	"github.com/diamondburned/arikawa/v2/bot"
+	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	token := os.Getenv("YADA_TOKEN")
 
-	rand.Seed(time.Now().UTC().UnixNano())
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatalln("Couldn't create discord session!", err)
+	}
 
-	bot.Run(
-		os.Getenv("YADA_TOKEN"),
-		&internal.Bot{},
-		func(ctx *bot.Context) error {
-			ctx.HasPrefix = bot.NewPrefix("!")
-			return nil
-		},
-	)
+	discord.Identify.Intents = discordgo.IntentsGuildMessages
+
+	discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commands.Handlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatalln("Couldn't open websocket connection to discord!", err)
+	}
+
+	for _, v := range commands.Commands {
+		fmt.Println(v.Name)
+		_, err = discord.ApplicationCommandCreate(discord.State.User.ID, "405287350298738689", v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	discord.Close()
 }
