7d3120cc5ef9768a514d9b5218ffaca700fc29fe
Vitaly Karpenko <vitkarpenko@gmail.com>
2022-03-19 21:17:58 +0300

    Removed unneeded config vars.

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,10 +5,4 @@ type Config struct {
 	Token           string `envconfig:"YADA_TOKEN"`
 	GuildID         string `envconfig:"YADA_GUILD_ID"`
 	ImagesChannelID string `envconfig:"YADA_IMAGES_CHANNEL_ID"`
-	DatabaseURL     string `envconfig:"DATABASE_URL"`
-	Quotes          Quotes
-}
-
-type Quotes struct {
-	QuotesChannelID string `envconfig:"YADA_QUOTES_CHANNEL_ID"`
 }
