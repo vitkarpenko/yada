94c1630228e0b0b4793363053c2d2866b7d2bbce
Vitaly Karpenko <vitkarpenko@gmail.com>
2022-05-16 12:10:42 +0300

    Major refactoring.

--- internal/utils/wait.go
+++ internal/utils/wait.go
@@ -1,4 +1,4 @@
-package bot
+package utils
 
 import (
 	"fmt"
@@ -8,7 +8,7 @@ import (
 )
 
 // waitUntilInterrupted waits  until CTRL-C or other term signal is received.
-func waitUntilInterrupted() {
+func WaitUntilInterrupted() {
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	waitChannel := make(chan os.Signal, 1)
 	signal.Notify(waitChannel, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
