66b3d30849f2038c7770c8204f6c622119fe9359
Vitaly Karpenko <vitkarpenko@gmail.com>
2022-07-27 14:41:13 +0300

    Gachi sounds slash command! Oh shit I'm sorry...

--- go.mod
+++ go.mod
@@ -5,19 +5,18 @@ go 1.18
 
 require (
 	github.com/bwmarrin/discordgo v0.25.0
+	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/mattn/go-sqlite3 v1.14.14
+	github.com/pressly/goose/v3 v3.6.1
 )
 
 require (
-	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/mattn/go-sqlite3 v1.14.14 // indirect
-	github.com/pressly/goose/v3 v3.6.1 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
-	golang.org/x/sys v0.0.0-20220429233432-b5fbb4746d32 // indirect
+	github.com/lithammer/fuzzysearch v1.1.5 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20220727055044-e65921a090b8 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
---
+++ internal/services/gachi/gachi.go
@@ -0,0 +1,152 @@
+package gachi
+
+import (
+	"bytes"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/lithammer/fuzzysearch/fuzzy"
+)
+
+const maxAutocompleteOpts = 25
+
+type Service struct {
+	dataPath string
+	sounds   []string
+}
+
+func New(dataPath string) *Service {
+	service := &Service{
+		dataPath: dataPath,
+	}
+	service.setSounds()
+	return service
+}
+
+func (s *Service) Handler(discord *discordgo.Session, i *discordgo.InteractionCreate) {
+	switch i.Interaction.Type {
+	case discordgo.InteractionApplicationCommandAutocomplete:
+		options := autocompleteOpts(i, s)
+		if len(options) == 0 {
+			return
+		}
+		sendAutocomplete(discord, i, options)
+	case discordgo.InteractionApplicationCommand:
+		fileName := i.ApplicationCommandData().Options[0].StringValue()
+		file, err := os.ReadFile(filepath.Join(s.dataPath, fileName))
+		if err != nil {
+			sendSoundNotFound(discord, i, fileName)
+			return
+		}
+		sendSound(discord, i, fileName, file)
+	}
+}
+
+func sendSound(
+	discord *discordgo.Session,
+	i *discordgo.InteractionCreate,
+	fileName string, file []byte,
+) {
+	_ = discord.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Files: []*discordgo.File{discordWavFromBytes(fileName, file)},
+		},
+	})
+}
+
+func sendSoundNotFound(
+	discord *discordgo.Session,
+	i *discordgo.InteractionCreate,
+	fileName string,
+) {
+	_ = discord.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: fmt.Sprintf("Не могу найти такой файл: %s... 🤔", fileName),
+		},
+	})
+}
+
+func autocompleteOpts(
+	i *discordgo.InteractionCreate, s *Service,
+) []*discordgo.ApplicationCommandOptionChoice {
+	query := i.ApplicationCommandData().Options[0].StringValue()
+	options := s.complete(query)
+	return options
+}
+
+func sendAutocomplete(
+	discord *discordgo.Session,
+	i *discordgo.InteractionCreate,
+	options []*discordgo.ApplicationCommandOptionChoice,
+) {
+	_ = discord.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionApplicationCommandAutocompleteResult,
+		Data: &discordgo.InteractionResponseData{
+			Choices: options,
+		},
+	})
+}
+
+func discordWavFromBytes(fileName string, data []byte) *discordgo.File {
+	file := &discordgo.File{
+		Name:        fileName,
+		ContentType: "audio/x-wav",
+		Reader:      bytes.NewReader(data),
+	}
+	return file
+}
+
+func (s *Service) complete(query string) []*discordgo.ApplicationCommandOptionChoice {
+	if len(query) == 0 {
+		return nil
+	}
+
+	matches := fuzzy.FindFold(query, s.sounds)
+	if len(matches) == 0 {
+		return nil
+	}
+
+	choices := make([]*discordgo.ApplicationCommandOptionChoice, len(matches))
+	for i, m := range matches {
+		choices[i] = &discordgo.ApplicationCommandOptionChoice{
+			Name:  m,
+			Value: m + ".wav",
+		}
+	}
+
+	if len(choices) > maxAutocompleteOpts {
+		choices = choices[:maxAutocompleteOpts]
+	}
+
+	return choices
+}
+
+func (s *Service) setSounds() {
+	filepath.WalkDir(
+		s.dataPath,
+		func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !d.IsDir() {
+				filename := filepath.Base(path)
+				splitted := strings.Split(filename, ".")
+				if len(splitted) != 2 || splitted[1] != "wav" {
+					log.Printf("Incorrect gachi sound file '%s', only .wav files are allowed", filename)
+					return nil
+				}
+				s.sounds = append(s.sounds, splitted[0])
+			}
+
+			return nil
+		},
+	)
+}
